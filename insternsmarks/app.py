3ef800674db220671b2b530cc7efa43034112394
Nilesh Raut <82454856+NileshRaut-code@users.noreply.github.com>
2021-11-12 13:47:52 +0530

    Update app.py

--- insternsmarks/app.py
+++ insternsmarks/app.py
@@ -1,23 +1,20 @@
 from flask import *
 from flask.templating import render_template
-from flask_sqlalchemy import SQLAlchemy 
 import sqlite3
 app=Flask(__name__)
 
 
- 
+
+
 @app.route('/')
 def index():
     connection = sqlite3.connect("intern_detials.db")
     connection.row_factory = sqlite3.Row
-    
     cursor = connection.cursor()
-    
-
     cursor.execute("select * from Int_Info ORDER BY id DESC")
     rows = cursor.fetchall()
     return render_template('index.html',rows=rows)
-    
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -25,12 +22,12 @@ def contact():
 @app.route("/contact",methods = ["POST"])
 def contacts():
     msg = "msg"
-    
+
     email=request.form["email"]
     name=request.form['name']
-    
+
     return (email)
-        
+
 
 @app.route('/about_us')
 def about():
@@ -39,7 +36,20 @@ def about():
 @app.route('/add')
 def add():
     return render_template('addpost.html')
-@app.route('/remove')
+@app.route('/asdfghjkl')
+def addsss():
+    return render_template('asdfghjkl.html')
+
+@app.route("/asdfghjkl",methods = ["POST"])
+def consssasd():
+    ids="Admin@12"
+    pas="Password"
+    id=request.form["id"]
+    passs=request.form["pass"]
+    if(id==ids and pas==passs):
+        return render_template('addpost.html')
+    return render_template('index.html')
+@app.route('/deleterecord')
 def rem():
     return render_template('remove.html')
 @app.route("/deleterecord",methods = ["POST"])
@@ -52,11 +62,9 @@ def deleterecord():
         rows = cursor.fetchall()
         if not rows == []:
 
-            cursor.execute("delete from Student_Info where id = ?",(id,))
-            cursor.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='Student_Info'")
+            cursor.execute("delete from Int_info where id = ?",(id,))
             msg = "Student detial successfully deleted"
-            return ('done')
-            
+            return render_template('success_record.html')
 
         else:
             msg = "can't be deleted"
@@ -64,8 +72,8 @@ def deleterecord():
 @app.route("/add",methods = ["POST"])
 def cons():
     msg = "msg"
-    
-    
+
+
     if request.method == "POST":
         try:
             title=request.form["title"]
@@ -73,15 +81,14 @@ def cons():
             des=request.form["des"]
 
             link=request.form["link"]
-            
 
-            
+
+
             with sqlite3.connect("intern_detials.db") as connection:
 
                 cursor = connection.cursor()
-               
+
                 cursor.execute("INSERT into Int_info (title,subtitle,des,link) values (?,?,?,?)",(title,subtitle,des,link))
-                
                 connection.commit()
                 msg = "Student detials successfully Added"
         except:
@@ -90,25 +97,22 @@ def cons():
             msg = "We can not add Student detials to the database"
         finally:
             return render_template("success_record.html",msg = msg)
-    
-@app.route     
+
+
+
 
 
 @app.route('/post/<int:id>')
 def post(id):
-    
+
     connection = sqlite3.connect("intern_detials.db")
 
     cursor = connection.cursor()
     cursor.execute("select * from Int_info where id=?", (id,))
     rows = cursor.fetchall()
     out = [item for t in rows for item in t]
-    print(out[0])
     print(out[1])
-    print(out[2])
-    print(out[3])
-
-    dict={        'title':out[1],        'subtitle':out[2]    ,    'link':out[4],        'des':out[2]    }
+    dict={        'title':out[1],        'subtitle':out[2]    ,    'link':out[4],        'des':out[3]    }
     return render_template('post.html', post=dict)
 if __name__=='__main__':
     app.run(debug=True)
